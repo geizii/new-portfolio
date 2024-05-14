9d6941d1c2c00d4c0b1f18aa9202a3bc3236341c
geizii <4663072@alunouninter.com>
2024-05-14 17:54:01 -0300

    :tada: commit inicial

---
+++ src/assets/js/reveal.js
@@ -0,0 +1,48 @@
+window.revelar = ScrollReveal({reset:true});
+
+revelar.reveal('.efeito-home-img', {
+    duration: 2000,
+    distance: '90px',
+    /* delay: 500 */
+})
+
+revelar.reveal('.efeito-home-title', {
+    duration: 2000,
+    distance: '90px',
+})
+
+revelar.reveal('.title', {
+    duration: 2000,
+    distance: '90px',
+})
+
+revelar.reveal('.efeito-skills-item', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// Section Projects
+
+revelar.reveal('.efeito-projects-box1', {
+    duration: 2000,
+    distance: '90px',
+})
+
+revelar.reveal('.efeito-projects-box2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-projects-box3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+revelar.reveal('.efeito-projects-box4', {
+    duration: 2000,
+    distance: '90px',
+    delay: 2000
+})
